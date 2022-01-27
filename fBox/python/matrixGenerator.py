820bd852fb138c375c1dde839094acd4e4cd43b0
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2022-01-27 18:57:44 +0100

    Update matrixGenerator.py

--- fBox/python/matrixGenerator.py
+++ fBox/python/matrixGenerator.py
@@ -3,19 +3,22 @@ import numpy as np
 
 
 def generateAdjacentMatrix(size,
-                            followChance, followBackChance,
-                            fraudGroupSize,
-                            fraudSpammerFollowChance, fraudSpammerFollowBackChance, fraudSpammerTarget  ):
-    matrix = np.zeros((size,size))
+                           followChance, followBackChance,
+                           fraudGroupSize,
+                           fraudSpammerFollowChance, fraudSpammerFollowBackChance, fraudSpammerTargetNumber):
+    matrix = np.zeros((size, size), np.int8)
     fraudSpammerCount = 0
     fraudGroupCount = 0
     groupIndexes = []
 
     for i in range(size):
+        if(i % 100 == 0):
+            print(f'i = {i}')
         # Case for spammer (following a lot of people without much followback)
-        if(fraudSpammerCount < fraudSpammerTarget and random() < fraudSpammerTarget/float(size)):
+        if(fraudSpammerCount < fraudSpammerTargetNumber and random() < fraudSpammerTargetNumber/float(size)):
             for j in range(size):
-                if(i == j): continue
+                if(i == j):
+                    continue
                 if(random() <= fraudSpammerFollowChance):
                     matrix[i][j] = 1
                     if(random() <= fraudSpammerFollowBackChance):
@@ -26,7 +29,8 @@ def generateAdjacentMatrix(size,
             fraudGroupCount += 1
         else:
             for j in range(size):
-                if(i == j): continue
+                if(i >= j):
+                    continue
                 if(random() <= followChance):
                     matrix[i][j] = 1
                     if(random() <= followBackChance):
@@ -35,12 +39,12 @@ def generateAdjacentMatrix(size,
     # Link all members of fraud group together
     for i in groupIndexes:
         for j in groupIndexes:
-            if(i == j): continue
+            if(i == j):
+                continue
             matrix[i][j] = 1
 
-    print(groupIndexes)
-    print(matrix[groupIndexes[0]])
     return matrix
 
-# size = 1000
-# print(generateAdjacentMatrix(size,0.05, 0.60, float(size)*0.05, 0.65, 0.02, float(size)*0.02))
+
+size = 10000
+print(generateAdjacentMatrix(size, 0.05, 0.90, 200, 0.95, 0.02, 50))
